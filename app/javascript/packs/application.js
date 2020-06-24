ae3d2e20ae69cad007e84cc9362af3dd23740ce9
Jonathan Sli <seriedigital@gmail.com>
2020-06-24 15:52:55 -0300

    v05sec9 problem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,6 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery")
 
 // BUG: Fix a Problem with jqury close messages  
 // Uncomment to copy all static images under ../images to the output folder and reference
