6defe251556b37706e783982d5fcb86f6068862d
Jonathan Sli <seriedigital@gmail.com>
2020-06-26 10:59:16 -0300

    add dotenv to proteck apikeys

---
+++ app/controllers/stocks_controller.rb
@@ -0,0 +1,7 @@
+# NOTE: I need to add a api key , and dotenv to protect the api key https://iexcloud.io/ user email j28... 
+# TODO: ASAP api key and env https://dev.to/torianne02/protecting-your-api-keys-rails-355k ; https://github.com/bkeepers/dotenv
+class StocksController < ApplicationController
+  def search
+
+  end
+end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,6 @@
 class Stock < ApplicationRecord
+  def self.new_form_lookup(ticker_symbol)
+    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+    new(name: looked_up_stock.name, ticker: looked_up_stock.symbol)
+  end
 end
