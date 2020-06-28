4cd75e31b0f6ba63a2993c03ac622d6456bd98f3
Jonathan Sli <seriedigital@gmail.com>
2020-06-28 01:39:37 -0300

    ending until 20 sec 9  and fix jquery

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,23 @@
 # NOTE: I need to add a api key , and dotenv to protect the api key https://iexcloud.io/ user email j28... 
-# TODO: ASAP api key and env https://dev.to/torianne02/protecting-your-api-keys-rails-355k ; https://github.com/bkeepers/dotenv
 class StocksController < ApplicationController
   def search
-
+    if params[:stock].present?
+      # Dotenv::Railtie.load
+      StockQuote::Stock.new(api_key: ENV["API_KEY_STOCK"])
+      @stock = Stock.new_form_lookup(params[:stock])
+      if @stock
+        # NOTE: this to use AJAX 
+        respond_to do |format|
+          format.js {render partial: 'users/result'}
+      end
+      else
+        flash[:danger] = "You have entered on empty search string"
+        redirect_to my_portfolio_path
+      end
+    else 
+      flash[:danger] = "You have entered on empty search string BBBBB:"
+      redirect_to my_portfolio_path
+    end
   end
 end
+ 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,21 +7,31 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+    // BUG: Fix a Problem with jquery close messages  
+    // Uncomment to copy all static images under ../images to the output folder and reference
+    // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+    // or the `imagePath` JavaScript helper below.
+    //
+    // const images = require.context('../images', true)
+    // const imagePath = (name) => images(name, true)
+    //= require rails-ujs
+    //= require jquery
+    //= require twitter/bootstrap
+    //= require_tree .
+    // ISSUE: it's doesn't work the selectors
+    // document.addEventListener("DOMContentLoaded", function() {
+    //     $('button.close').on('click', function() {
+    //         console.log('hola Mundo');
+    //     })
+    // })
 
-// BUG: Fix a Problem with jquery close messages  
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-//= require rails-ujs
-//= require jquery
-//= require twitter/bootstrap
-//= require_tree .
-// ISSUE: it's doesn't work the selectors
-// document.addEventListener("DOMContentLoaded", function() {
-//     $('button.close').on('click', function() {
-//         console.log('hola Mundo');
-//     })
-// })
+$(document).ready(function() {
+    $('button.close').click(function() {
+            $('.alert').removeClass('in').addClass('out')
+        })
+        // $('#stock-lookup-form').on('ajax:complete', function(event, data, status) {
+        //     console.log(data);
+        //     $('#results').html(data.responseText);
+        // })
+})
